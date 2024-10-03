d7d0e66fbeb267bf9b0764717864dc970e430cdb
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-03 15:24:58 +0530

    component created & requests sent

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,6 +1,15 @@
+import { Auth } from "../Components/Auth"
+import { Quote } from "../Components/Quote"
 
 export const Signin = () => {
     return <div>
-        Signin
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type="signin" />
+            </div>
+            <div className="hidden lg:block">
+                <Quote/>
+            </div>
+        </div>
     </div>
 }
