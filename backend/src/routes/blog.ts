0f3aacec73e492a3f720aba48b8f09f588b459c4
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-04 20:33:52 +0530

    better BE

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -96,7 +96,18 @@ blogRouter.get('/bulk', async (c) => {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate())
-    const blogs = await prisma.blog.findMany();
+    const blogs = await prisma.blog.findMany({
+        select: {
+            content: true,
+            title: true,
+            id: true,
+            author: {
+                select: {
+                    name: true
+                }
+            }
+        }
+    });
 
     return c.json({
         blogs
