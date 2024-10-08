74f2a6f4fe8763473e95f7a678174c94c95dbc16
Mohammed Zaid <mohammedzee98@gmail.com>
2024-10-08 16:22:09 +0530

    axios error handled

--- frontend/src/Hooks/index.ts
+++ frontend/src/Hooks/index.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../config";
-
 export interface Blog {
   content: string;
   title: string;
@@ -51,3 +50,20 @@ export const useBlogs = () => {
   }, []);
   return { loading, blogs };
 };
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { BACKEND_URL } from "../config";
+
+// export const useBlogs = () => {
+//   const [loading, setLoading] = useState(true);
+//   const [blogs, setBlogs] = useState([]);
+
+//   useEffect(() => {
+//     axios.get(`${BACKEND_URL}/api/v1/blog/bulk`).then((response) => {
+//       setBlogs(response.data);
+//       setLoading(false);
+//     });
+//   }, []);
+
+//   return { loading, blogs };
+// };
